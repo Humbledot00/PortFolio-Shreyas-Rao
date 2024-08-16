dc9c3699f76ac4e248f8ba148b44df74d5d7f995
Shreyas Rao <109731624+Humbledot00@users.noreply.github.com>
2024-08-16 22:23:41 +0530

    Update Cerificate.jsx

--- src/components/Cerificates/Cerificate.jsx
+++ src/components/Cerificates/Cerificate.jsx
@@ -74,7 +74,8 @@ const posts = [
                   </time>
                   <a
                     className="relative z-10 rounded-full bg-gray-50 px-3 py-1.5 font-medium text-gray-600 hover:bg-gray-100"
-                  >
+                    href={post.href}
+                >
                     {post.category.title}
                   </a>
                 </div>
